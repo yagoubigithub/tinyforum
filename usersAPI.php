0c6f1351ece0d40d6a64180f4634e5c4e0a30eac
Yagoubi Aek <yagoubi.aek.2@gmail.com>
2018-06-19 23:19:08 +0100

    tinyf_users_update
    change a user information by his id you can change the name or the email or password or all

--- usersAPI.php
+++ usersAPI.php
@@ -1,4 +1,28 @@
 <?php
+
+
+/***********************************************************
+$tf_host='localhost';
+$tf_dbname='tinyforum';
+$tf_username = 'root';
+$tf_password = '';
+
+
+
+$tf_handle = mysqli_connect($tf_host, $tf_username, $tf_password,$tf_dbname);
+
+// Check connection
+if (!$tf_handle) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+//die('OK');
+//@mysqli_close($tf_handle);
+mysqli_query($tf_handle,"SET NAMES 'utf8");
+
+****************************************************************/ 
+
+
 function tinyf_users_get($extra = ''){
     global $tf_handle;
     $query=sprintf("SELECT * FROM `users` %s",$extra);
@@ -13,10 +37,10 @@ function tinyf_users_get($extra = ''){
     return NULL;
 
     $users = array();
-    for($i = 0; i < $rcount; $i++){
+    for($i = 0; $i < $rcount; $i++){
        $users[count($users)]= mysqli_fetch_object($qresult);
     }
-    $mysqli_free_result($qresult);
+    mysqli_free_result($qresult);
     return $users;
 }
 
@@ -26,11 +50,12 @@ function tinyf_users_get_by_id($uid){
 
     if($id == 0)
     return NULL;
- $result = tinyf_users_get('WHERE id = '.$uid);
+ $result = tinyf_users_get('WHERE id = '.$id);
  if($result == NULL){
      return NULL;
  }
  $user = $result[0];
+ return $user;
 
 }
 
@@ -75,5 +100,66 @@ function tinyf_users_delete($uid){
      
     return true;
 }
-tinyf_users_delete(1);
+
+
+function tinyf_users_update($uid,$name = NULL,$password = NULL,$email = NULL,$isadmin = 0){
+    global  $tf_handle;
+    $id= (int)$uid;
+    $n_isadmin = (int)$isadmin;
+    if($id == 0){
+        echo "is zero";
+        return false;
+    }
+    $user = tinyf_users_get_by_id($id);
+    if(!$user){
+        echo "no user with this id";
+        return false;
+    }
+    if((empty($name))  && (empty($password)) && (empty($email)) && ($user->isadmin == $n_isadmin)){
+
+        return false;
+    }
+    $fields =array();
+    $query = 'UPDATE `users` SET ';
+
+    if((!empty($name))){
+        $n_name = mysqli_real_escape_string($tf_handle,strip_tags($name));
+        $fields[count($fields)] = " `name` = '$n_name'";
+    }
+    if((!empty($password))){
+        $n_pass = md5(mysqli_real_escape_string($tf_handle,strip_tags($password)));
+        $fields[count($fields)] = " `password` = '$n_pass'";
+    }
+    if((!empty($email))){
+        $n_email = mysqli_real_escape_string($tf_handle,strip_tags($email));
+        $fields[count($fields)] = " `email` = '$n_email'";
+    }
+    $fields[count($fields)] = " `isadmin` = '$isadmin'";
+
+    $fcount =count($fields);
+    if($fcount == 1){
+        $query .= $fields[0]." WHERE `id` = ".$id;
+        $qresult = mysqli_query($tf_handle,$query);
+        if(!$qresult){
+            return false;
+        }else{
+            return true;
+        }
+    }
+    for($i = 0;$i<$fcount;$i++){
+        $query.=$fields[$i].' ';
+        if($i != ($fcount -1 )){
+            $query .= ' , ';
+
+        }
+    }
+    $query .= " WHERE `id` = ".$id;
+    $qresult = mysqli_query($tf_handle,$query);
+    if(!$qresult){
+        return false;
+    }else{
+        return true;
+    }
+}
+
 ?>
